1224c3b7323f966341036cf2a7d2935c6c1c8783
afrodos <30568900+afrodos@users.noreply.github.com>
2021-03-26 11:45:34 +0100

    default form

---
+++ src/Form.java
@@ -0,0 +1,6 @@
+import javax.swing.JFrame;
+
+public class Form extends JFrame {
+    private static final long serialVersionUID = -1450150616029361695L;
+    
+}
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    private static Form form1;
+    public static void main(String[] args) {
+        form1 = new Form();
+        form1.setVisible(true);
+    }
+}
