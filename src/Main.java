4e1023ce28f66ee7b06f10a998ea7c1b89fbd366
afrodos <30568900+afrodos@users.noreply.github.com>
2021-04-23 08:46:13 +0200

    newcode

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
 public class Main {
     private static HomeForm form1;
     public static void main(String[] args) {
-        form1 = new HomeForm();
-        form1.setVisible(true);
+        // form1 = new HomeForm();
+        // form1.setVisible(true);
+
+        OntwerperForm o = new OntwerperForm();
+        o.setVisible(true);
 
     }
 }
--- src/OntwerperForm.java
+++ src/OntwerperForm.java
@@ -1,5 +1,70 @@
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
-public class OntwerperForm extends JFrame {
-    
+public class OntwerperForm extends JFrame implements ActionListener{
+    private GridBagConstraints gbc = new GridBagConstraints();
+
+    //buttons etc
+    private JButton opslaan;
+    private JButton ontwerpLaden;
+    private JButton terugKnop;
+    ArrayList<JPanel> components = new ArrayList<JPanel>(); 
+
+    public OntwerperForm(){
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("HOME");
+        setBounds(0, 0, 650, 700);
+        //setLayout(new GridBagLayout());
+
+        JPanel panel = new JPanel(new GridBagLayout());
+        panel.setBackground(Color.blue);
+        add(panel, BorderLayout.NORTH);
+
+        gbc.insets = new Insets(20,5,0,5);
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        panel.add(new JLabel("Server Ontwerper"),gbc);
+        gbc.gridx = 1;
+        opslaan = new JButton("Ontwerp opslaan");
+        opslaan.addActionListener(this);
+        panel.add(opslaan,gbc);
+        gbc.gridx = 2;
+        ontwerpLaden = new JButton("Ontwerp laden");
+        ontwerpLaden.addActionListener(this);
+        panel.add(ontwerpLaden,gbc);
+        gbc.gridx = 3;
+        terugKnop = new JButton("Terug");
+        terugKnop.addActionListener(this);
+        panel.add(terugKnop,gbc);
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        gbc.gridheight = 4;
+        gbc.fill = GridBagConstraints.VERTICAL;
+        JPanel sidePanel = new JPanel(new GridLayout(2,0));
+        sidePanel.setBackground(Color.pink);
+        sidePanel.setPreferredSize(new Dimension(200,0));
+        add(sidePanel, BorderLayout.WEST);
+
+        JPanel compListPanel = new JPanel(new GridLayout(0, 2));
+        compListPanel.setBackground(Color.GREEN);
+        compListPanel.setSize(new Dimension(200,0));
+        
+        
+        sidePanel.add(compListPanel);
+        sidePanel.add(new JLabel("BESCHIKBAARHEID: 0%"));
+
+        JPanel tekenPanel = new JPanel();
+        tekenPanel.setBackground(Color.YELLOW);
+        add(tekenPanel, BorderLayout.CENTER);
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
 }
