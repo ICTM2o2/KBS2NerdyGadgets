adb5ce6e96a2f0243d39f74c27918322e128b7dc
afrodos <30568900+afrodos@users.noreply.github.com>
2021-05-10 15:17:21 +0200

    python script

---
+++ pythonscripts/DBinsert.py
@@ -0,0 +1,32 @@
+import psutil #pip install psutil
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="yourusername",
+  password="yourpassword",
+  database="mydatabase"
+)
+
+mycursor = mydb.cursor()
+
+hdd = psutil.disk_usage('/')
+# print("Total: %d GiB" % (hdd.total // (2**30)))
+# print("Used: %d GiB" % (hdd.used // (2**30)))
+# print("Free: %d GiB" % (hdd.free // (2**30)))
+
+# print(psutil.cpu_percent(interval=1))
+
+sql = "INSERT INTO stats (name, used, free, cpu) VALUES (%s, %d, %d, %d)"
+val = ("DB01", (hdd.used // (2**30)),(hdd.free // (2**30)), psutil.cpu_percent(interval=1))
+mycursor.execute(sql, val)
+
+mydb.commit()
+
+
+#print(psutil.cpu_percent(interval=4))#get cpu usage for 4 seconds
+
+#harddrive
+#cpu
+#beschikbaarheid
+#online offline
