28e9414f492f4266614fd71bdd4fa1159d03bf44
vitomendez <vitomendez@live.com>
2021-05-11 12:46:43 +0200

    test

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,13 @@
 public class Main {
-    private static HomeForm a;
+    private static HomeForm form1;
     public static void main(String[] args) {
-         //HomeForm a = new HomeForm();
-         //a.setVisible(true);
+        form1 = new HomeForm();
+        form1.setVisible(true);
 
-         OptimalisatieForm b = new OptimalisatieForm();
-         b.setVisible(true);
+        OntwerperForm o = new OntwerperForm();
+        o.setVisible(true);
 
-         OntwerperForm o = new OntwerperForm();
-         o.setVisible(true);
-
+        OptimalisatieForm p = new OptimalisatieForm();
+        p.setVisible(true);
     }
 }
