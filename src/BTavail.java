2b5ec19c07130e7f1bedf57cc0f015d800593944
afrodos <30568900+afrodos@users.noreply.github.com>
2021-05-21 11:34:26 +0200

    werkt maar sloom

--- src/BTavail.java
+++ src/BTavail.java
@@ -4,47 +4,57 @@ import java.util.ArrayList;
 public class BTavail {
     public static void main(String[] args) {
         ArrayList<servComponent> allComps = new ArrayList<>();
-        servComponent f = new servComponent(99.99, "pfsense", servComponent.serverType.FIREWALL, 7000);
-        servComponent w = new servComponent(80.00, "web01", servComponent.serverType.WEB, 7000);
-        servComponent d = new servComponent(80.00, "db01", servComponent.serverType.DATABASE, 7000);
+        servComponent f = new servComponent(99.998, "pfsense", servComponent.serverType.FIREWALL, 4000);
+        servComponent w = new servComponent(80.00, "web01", servComponent.serverType.WEB, 2200);
+        servComponent w1 = new servComponent(90.00, "web02", servComponent.serverType.WEB, 3200);
+        servComponent d = new servComponent(90.00, "db01", servComponent.serverType.DATABASE, 5100);
+        servComponent d1 = new servComponent(95.00, "db02", servComponent.serverType.DATABASE, 7700);
         allComps.add(f);
         allComps.add(w);
         allComps.add(d);
-        solve(allComps, new ArrayList<servComponent>(), Integer.MAX_VALUE, 99.99, 70000, 0, 1000);
+        allComps.add(w1);
+        allComps.add(d1);
+        solve(allComps, new ArrayList<servComponent>(), 99.99, 70000, 15);
     }
 
+    public static void printComps(ArrayList<servComponent> components){
+        //System.out.println("========");
+        System.out.println("price: "+calcPrice(components));
+        // for (servComponent c : components) {
+        //     System.out.println(c.getName());
+        // }
+        // System.out.println("========");
+    }
 
-    public static boolean solve(ArrayList<servComponent> allComps, ArrayList<servComponent> currentComps, int currentLowestPrice, double desiredAvailability, int hardPriceLimit, int currentStepCount, int hardCompLimit){
+    public static int curlow = Integer.MAX_VALUE;
 
-        if (currentStepCount >= hardCompLimit){
-            return false;
-        }
-        else if (isValid(currentComps, desiredAvailability, hardPriceLimit)){
-            for (servComponent c : currentComps) {
-                System.out.println(c.getName());
-            }
-            return true;
+    public static void solve(ArrayList<servComponent> allComps, ArrayList<servComponent> currentComps, double desiredAvailability, int hardPriceLimit, int hardCompLimit){
+        if (isSolution(currentComps, desiredAvailability) && calcPrice(currentComps) < curlow && calcPrice(currentComps) <= hardPriceLimit){
+            curlow = calcPrice(currentComps);
+            printComps(currentComps);
+            return;
         }
 
         for (servComponent servComponent : allComps) {
             ArrayList<servComponent> temp = new ArrayList<>();//create temporary list
             temp.addAll(currentComps);//add all previous things in tree
             temp.add(servComponent);
-            if (!isValid(temp, desiredAvailability, hardPriceLimit)){
-                if(solve(allComps, temp, currentLowestPrice, desiredAvailability, hardPriceLimit, currentStepCount++, hardCompLimit)){
-                    System.out.println("--------awnser------");
-                    for (servComponent c : temp) {
-                        System.out.println(c.getName());
-                    }
-                    System.out.println("--------------------");
-                    
-                }
+            if (temp.size() <= hardCompLimit){
+                solve(allComps, temp, desiredAvailability, hardPriceLimit, hardCompLimit);//recurse
             }
         }
-        return false;
+        
     }
 
-    public static boolean isValid(ArrayList<servComponent> currentComps, double desiredAvailability, int hardPriceLimit){
+    public static int calcPrice(ArrayList<servComponent> currentComps){
+        int total = 0;
+        for (servComponent c : currentComps) {
+            total += c.getPrice();
+        }
+        return total;
+    }
+
+    public static boolean isSolution(ArrayList<servComponent> currentComps, double desiredAvailability){
         ArrayList<servComponent> webs = new ArrayList<>();
         ArrayList<servComponent> dbs = new ArrayList<>();
         ArrayList<servComponent> fws = new ArrayList<>();
@@ -85,13 +95,11 @@ public class BTavail {
         double web = new BigDecimal("1").subtract(webAv).multiply(new BigDecimal("100")).doubleValue();
         double db = new BigDecimal("1").subtract(dbAv).multiply(new BigDecimal("100")).doubleValue();
         double fw = new BigDecimal("1").subtract(fwAv).multiply(new BigDecimal("100")).doubleValue();
-        
-        System.out.println("fw: "+fw);
-        System.out.println("web: "+web);
-        System.out.println("db: "+db);
-        System.out.println("total: "+total);
-
-        return !(total < desiredAvailability || web < desiredAvailability || db < desiredAvailability || fw < desiredAvailability);
+        // System.out.println("total: " + total);
+        // System.out.println("web: " + web);
+        // System.out.println("db: " + db);
+        // System.out.println("fw: " + fw);
+        return (total >= desiredAvailability && web >= desiredAvailability && fw >= desiredAvailability && db >= desiredAvailability);
         
     }
 }
